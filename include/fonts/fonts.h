da47cc162ca852aa98ce8ea6c2350def3c6e3499
Alex Brooke <akb.sbc@gmail.com>
2018-10-02 15:13:28 -0400

    declare fonts properly

--- include/fonts/fonts.h
+++ include/fonts/fonts.h
@@ -21,21 +21,21 @@ LV_FONT_DECLARE(pros_font_yu_gothic_ui_cjk1);
  * CJK Unified Ideographs, Part 2 of 4 (unicode 0x6300-0x77FF)
  */
 #ifdef USE_PROS_FONT_YU_GOTHIC_UI_CJK2
-LV_FONT_DECLARE(pros_font_yu_gothic_ui_cjk1);
+LV_FONT_DECLARE(pros_font_yu_gothic_ui_cjk2);
 #endif
 
 /**
  * CJK Unified Ideographs, Part 3 of 4 (unicode 0x7800-0x8CFF)
  */
 #ifdef USE_PROS_FONT_YU_GOTHIC_UI_CJK3
-LV_FONT_DECLARE(pros_font_yu_gothic_ui_cjk1);
+LV_FONT_DECLARE(pros_font_yu_gothic_ui_cjk3);
 #endif
 
 /**
  * CJK Unified Ideographs, Part 4 of 4 (unicode 0x8D00-0x9FFF)
  */
 #ifdef USE_PROS_FONT_YU_GOTHIC_UI_CJK4
-LV_FONT_DECLARE(pros_font_yu_gothic_ui_cjk1);
+LV_FONT_DECLARE(pros_font_yu_gothic_ui_cjk4);
 #endif
 
 #endif
